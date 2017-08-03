7e9aa39a7d39e9fcbef47fe165bf2edcde380eac
Adam Mohammad <emperor.adam@gmail.com>
2017-08-03 14:38:06 +0200

    R code for obtaining the results (version for HPC)
    Code for fitting joint models and calculating their predictive performance (version for HPC cluster, Linux)

---
+++ Simulation3_HPC.r
@@ -0,0 +1,79 @@
+# Master Thesis KU Leuven
+# Adam Mohammad
+# Title: A Study on the Effect of Misspecifying the Association Structure in Dynamic Predictions 
+# Obtained from Joint Models for Longitudinal and Survival Data
+# August 2017
+# Code for fitting joint models and calculating their predictive performance (version for HPC cluster, Linux)
+
+rm(list = ls())
+library(doParallel)
+library(nlme)
+library(survival)
+library(JMbayes)
+library(splines)
+
+
+load("/home/mohammad/Comp/Alldata.RData")
+source("/home/mohammad/Comp/functions.r")
+
+#### PE, AUC Calculation####
+n.burnin=10000L
+n.iter=200000L
+n.adapt=10000L
+M=200L
+time_points=c(5.5,7.5,9.5,11.5,13.5,15.5)
+
+lmcont=lmeControl(opt="optim", maxIter = 1000L,msMaxIter = 1000L)
+
+
+dForm = list(fixed = ~ 0 + group:dns(time, knots=c(2.5, 9), B = c(0, 25)), 
+             indFixed = 4:9,random = ~ 0 + dns(time, knots=c(2.5, 9), B = c(0, 25)), 
+             indRandom = 2:4)
+
+iForm = list(fixed = ~ 0 + group:time +Covariate:time + group:ins(time, knots=c(2.5, 9), B = c(0, 25)),
+             indFixed = 1:9, random = ~ 0 + time + ins(time, knots=c(2.5, 9), B = c(0, 25)), 
+             indRandom = 1:4)
+
+
+data1=data3
+
+
+r=as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID")) # get id of the job
+
+
+result=list()
+dat=data1[[r]]
+
+lm=lme(y ~ 0 + group + Covariate+ group:ns(time, knots=c(2.5, 9), B = c(0, 25)), 
+       random=list(id=pdDiag(~1+ns(time, knots=c(2.5, 9), B = c(0, 25)))),
+       data=dat$train_data, method = "REML",control =lmcont)
+
+sm=coxph(Surv(TimeEv, event) ~ group+Covariate, data = dat$train_data2, x = TRUE, model=TRUE)
+
+joint1=jointModelBayes(lmeObject = lm,survObject = sm,timeVar = "time",
+                       control =list(n.iter=n.iter,n.burnin=n.burnin,n.adapt=n.adapt,keepRE=TRUE) )
+
+joint2=jointModelBayes(lmeObject = lm,survObject = sm,timeVar = "time",
+                       param = "td-both",extraForm = dForm,
+                       control =list(n.iter=n.iter,n.burnin=n.burnin,n.adapt=n.adapt,keepRE=TRUE) )
+
+joint3=jointModelBayes(lmeObject = lm,survObject = sm,timeVar = "time",
+                       param = "td-extra", extraForm = iForm,
+                       control =list(n.iter=n.iter,n.burnin=n.burnin,n.adapt=n.adapt,keepRE=TRUE) )
+
+result_model1=PeAuc(object=joint1,newdata = dat$test_data,timepoints = time_points,idVar = "id", M=M )
+result_model2=PeAuc(object=joint2,newdata = dat$test_data,timepoints = time_points,idVar = "id", M=M )
+result_model3=PeAuc(object=joint3,newdata = dat$test_data,timepoints = time_points,idVar = "id", M=M )
+result_landmarking=PeAucCox(sm,dat$test_data,time_points)
+
+result=list(result_model1=result_model1,result_model2=result_model2,result_model3=result_model3,
+            result_landmarking=result_landmarking,time_model1=joint1$time[3],time_model2=joint2$time[3],
+            time_model3=joint3$time[3],name=dat$name)
+
+
+outputName=paste("output",r,".RData",sep="")
+outputPath=file.path("/home/mohammad/Comp",outputName) 
+save(result,file=outputPath)
+  
+
+
